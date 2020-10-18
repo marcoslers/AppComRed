d54aea1da20f936b433e0721541e204e06ab92c1
Marcos Leon <marcosleonrs@gmail.com>
2020-10-18 16:23:01 -0500

    resp

--- programa4.c
+++ programa4.c
@@ -3,13 +3,29 @@
 #include <pthread.h>
 #include <string.h>
 
-typedef struct data{
+typedef struct matrix{
+    int **m;
+    int r;
+    int c;
+}matrix;
 
-}data;
+typedef struct package{
+    matrix *m1;
+    matrix *m2;
+    matrix *mrx;
+    int l,h;
+}package;
 
-void * funcion(void * parametro){
+void * function(void * parametro){
 
-   
+    package *pkg = (package*)parametro;
+
+    printf("l: %d\n",pkg->l);
+    printf("h: %d\n",pkg->h);
+
+    //pkg->m1->m[0][0]=55;
+    //pkg->m1->m[1][0]=65;
+       
 }
 
 int main(int argc, char *argv[]){
@@ -17,64 +33,97 @@ int main(int argc, char *argv[]){
     time_t t;
     srand((unsigned) time(&t));
 
-    int t1r,t1c,t2r,t2c,nt,rxt,r;
+    matrix m1;
+    matrix m2;
+    matrix mrx;
+
+    int nt,rxt,r,l=0,h=0;
 
     if(argc!=6){
         printf("parametros incompletos\n");
         exit(0);
     }
-    t1r=atoi(argv[1]);
-    t1c=atoi(argv[2]);
-    t2r=atoi(argv[3]);
-    t2c=atoi(argv[4]);
+
+    m1.r=atoi(argv[1]);
+    m1.c=atoi(argv[2]);
+    m2.r=atoi(argv[3]);
+    m2.c=atoi(argv[4]);
+    mrx.r=m1.r;
+    mrx.c=m2.c;
+
     nt=atoi(argv[5]);
 
-    if(nt<1 || nt>t1r){
+    if(nt<1 || nt>m1.r){
         printf("error en el # de hilos");
         exit(0);
-    }else if(t1c!=t2r){
+    }else if(m1.c!=m2.r){
         printf("error en la longitud de las tablas");
         exit(0);        
     }
 
-    int **m1 = (int **)malloc(sizeof(int*)*t1r);
-    int **m2 = (int **)malloc(sizeof(int*)*t2r);
-    int **mr = (int **)malloc(sizeof(int*)*t1r);
+    m1.m = (int **)malloc(sizeof(int*)*m1.r);
+    m2.m = (int **)malloc(sizeof(int*)*m2.r);
+    mrx.m = (int **)malloc(sizeof(int*)*mrx.r);
 
-    for(int i=0;i<t1r;i++){
-        m1[i]=(int*)malloc(sizeof(int)*t1c);
-        for(int j=0;j<t1c;j++){
+    for(int i=0;i<m1.r;i++){
+        m1.m[i]=(int*)malloc(sizeof(int)*m1.c);
+        for(int j=0;j<m1.c;j++){
             int r=rand()%6;
-            m1[i][j]=r;
-            //printf("%d ",m1[i][j]);
+            m1.m[i][j]=r;
+           // printf("%d ",m1.m[i][j]);
         }
         //printf("\n");
     }
 
-    for(int i=0;i<t2r;i++){
-        m2[i]=(int*)malloc(sizeof(int)*t2c);
-        for(int j=0;j<t2c;j++){
+    for(int i=0;i<m2.r;i++){
+        m2.m[i]=(int*)malloc(sizeof(int)*m2.c);
+        for(int j=0;j<m2.c;j++){
             int r=rand()%6;
-            m2[i][j]=r;
-            //printf("%d ",m2[i][j]);
+            m2.m[i][j]=r;
+          //  printf("%d ",m2.m[i][j]);
         }
         //printf("\n");
     }
 
-    for(int i=0;i<t1r;i++){
-        mr[i]=(int*)malloc(sizeof(int)*t2c);
+    for(int i=0;i<mrx.r;i++){
+        mrx.m[i]=(int*)malloc(sizeof(int)*mrx.c);
     }  
 
-    rxt=t1r/nt;
-    r=t1r%nt;
+    rxt=m1.r/nt;
+    r=m1.r%nt;
 
     pthread_t *threads;
 
     threads = (pthread_t *)malloc(sizeof(pthread_t)*nt);
 
     for(int i=0;i<nt;i++){
-        pthread_create(&threads[i], NULL, function,(void*)&nth[i]);
+
+        if(i==0){
+            l=0;
+            h+=(rxt+r-1);
+        }else{
+            l+= rxt+r;
+            h+=(rxt+r-1);
+        }
+     
+        package pck;
+        pck.m1=&m1;
+        pck.m2=&m2;
+        pck.mrx=&mrx;
+        pck.l=l;
+        pck.h=h;
+
+        printf("%d %d\n",l,h);
+
+        //pthread_create(&threads[i], NULL, function,(void*)&pck);
     }
+
+    for(int i=0; i<nt; i++){
+
+        pthread_join(threads[i], NULL);
+    }
+
+    free(threads);
 
     exit(0);
 }
