3b77c599581484217cf7541b912feec064a11802
Marcos <marcosleonrs@gmail.com>
2020-12-12 19:37:56 -0600

    file transmition with any kind of file

---
+++ sockets/imgserver/client_img.c
@@ -0,0 +1,100 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netinet/ip.h>
+#include<string.h>
+#include<arpa/inet.h>
+
+#define PORT 7200
+#define MSGLEN 1500
+
+typedef struct{
+    int id;
+    char filename[15];
+    char msg[MSGLEN];
+}Package; 
+
+void sendMsg(int sockfd,char *filename){
+
+    int idMsg = 0;
+    char path[30]="./source/", en ='\0';
+    strcat(path,filename);
+    strncat(path,&en,1);
+
+    FILE *fp;
+    fp = fopen(path,"rb");
+
+    if(fp==NULL){
+        printf("error opening the file");
+        exit(0);
+    } 
+
+    Package pkg;
+
+    strcpy(pkg.filename,filename);
+
+    while(fread(pkg.msg,sizeof(char),MSGLEN,fp)>0){
+
+        pkg.id=idMsg++;
+
+        int nbytess=sendto(sockfd,&pkg,sizeof(Package),0,NULL,0);
+            
+        bzero(pkg.msg,MSGLEN); 
+
+        int nbytesr=recvfrom(sockfd,&pkg,sizeof(Package),0,NULL,NULL);
+
+        if(pkg.id!=idMsg){
+            printf("error sending package\n");
+            exit(0);
+        }
+
+        bzero(pkg.msg,MSGLEN);
+    }
+
+    pkg.id=-1;
+    int nbytess=sendto(sockfd,&pkg,sizeof(Package),0,NULL,0);
+
+    printf("%s uploaded\n",filename);
+
+    fclose(fp);
+}
+
+int main(int argc,char *argv[]){
+
+    int sockfd, connfd,flag=1; 
+    struct sockaddr_in server, client;
+      
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1) { 
+        printf("socket creation failed...\n"); 
+        exit(0); 
+    }
+
+    if(setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&flag,sizeof(flag))==-1){
+        printf("setsockopt failed...\n"); 
+        exit(0); 
+    }
+
+    bzero(&server, sizeof(server)); 
+  
+    server.sin_family = AF_INET; 
+    server.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    server.sin_port = htons(PORT);     
+    
+    if (connect(sockfd, (struct sockaddr*)&server, sizeof(server)) != 0) { 
+        printf("connection with the server failed...\n"); 
+        exit(0); 
+    }else{
+        printf("connected to the server..\n"); 
+    }
+
+    sendMsg(sockfd,argv[1]);
+
+    close(sockfd);
+    
+    return 0;
+}
---
+++ sockets/imgserver/server_img.c
@@ -0,0 +1,122 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netinet/ip.h>
+#include<pthread.h>
+
+#define PORT 7200
+#define MSGLEN 1500
+
+typedef struct{
+    int id;
+    char filename[15];
+    char msg[MSGLEN];
+}Package; 
+
+
+void *handler(void *args){
+
+    int *idCh = (int*)args, banFile=1;
+    char path[30]="./dest/";
+    FILE *fp;
+    Package pkg;
+    
+    while(1){
+
+        bzero(pkg.msg, MSGLEN); 
+
+        int nbytesr=recvfrom(*idCh,&pkg,sizeof(Package),0,NULL,NULL);
+
+        if(banFile){
+            strcat(path,pkg.filename);
+            fp = fopen(path, "a+");
+            if(fp==NULL){
+                printf("error opening the file");
+                exit(0);
+            }
+            banFile=0; 
+        }
+
+        if(pkg.id==-1){
+            printf("%s completed\n",pkg.filename);
+            break;
+        }
+
+        fwrite(pkg.msg,sizeof(char),MSGLEN,fp);
+
+        bzero(pkg.msg, MSGLEN);
+        pkg.id++;
+
+        int nbytess=sendto(*idCh,&pkg,sizeof(Package),0,NULL,0);
+
+    }
+
+    fclose(fp);
+    close(*idCh);
+    free(idCh);
+}
+
+int main(int argv,char *args[]){
+
+    int sockfd,connfd,len,flag=1; 
+    struct sockaddr_in server,client; 
+    
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+
+    if(sockfd == -1){ 
+        printf("socket creation failed...\n"); 
+        exit(0); 
+    }
+
+    if(setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&flag,sizeof(flag))==-1){
+        printf("setsockopt failed...\n"); 
+        exit(0); 
+    }
+
+    bzero(&server, sizeof(server)); 
+
+    server.sin_family = AF_INET; 
+    server.sin_addr.s_addr = htonl(INADDR_ANY); 
+    server.sin_port = htons(PORT); 
+
+    if ((bind(sockfd, (struct sockaddr*)&server, sizeof(server))) != 0) { 
+        printf("socket bind failed...\n"); 
+        exit(0); 
+    }
+
+    if((listen(sockfd, 3)) != 0) { 
+        printf("Listen failed...\n"); 
+        exit(0); 
+    }
+
+    len = sizeof(client);
+    
+    while(1){
+
+        int *idCh = (int*)malloc(sizeof(int));
+
+        *idCh = accept(sockfd,(struct sockaddr*)&client, &len);
+
+        if((*idCh)<0){ 
+            printf("server acccept failed...\n"); 
+            exit(0); 
+        }else{
+            printf("server acccept the client...\n"); 
+        }
+
+        pthread_t *thread = (pthread_t*)malloc(sizeof(pthread_t));
+
+        pthread_create(thread, NULL,(void*)handler,(void*)idCh);
+
+        free(thread);
+
+    }
+
+    close(sockfd);
+
+    return 0;
+}
